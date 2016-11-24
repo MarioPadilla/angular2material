efd6b87332d6b87559a81d1af72df3a676fb5858
Mario Padilla Carrión <mca@expandit.com>
2016-11-24 14:32:19 +0100

    Home Component: Connect with Firebase and add description

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  infos: FirebaseObjectObservable<any>;
+
+	constructor(af: AngularFire) {
+		this.infos = af.database.object('/team/infos');
+	}
 
   ngOnInit() {
   }
+
+  
 
 }
